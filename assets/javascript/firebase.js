8b69a3d830b16039d4a9cb9a936a74b6836bd492
Matt Wigdahl <matt@leverpulley.com>
2019-09-30 19:34:02 -0600

    started folder & file structure for project.

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCDKdUg5y9iSENnaHRpGx2czlfEyjOza6s",
+    authDomain: "uber-pub-crawl.firebaseapp.com",
+    databaseURL: "https://uber-pub-crawl.firebaseio.com",
+    projectId: "uber-pub-crawl",
+    storageBucket: "uber-pub-crawl.appspot.com",
+    messagingSenderId: "746444227672",
+    appId: "1:746444227672:web:5938a0ac6de7275e42362d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ assets/javascript/uber.js
@@ -0,0 +1,3 @@
+//Add Uber API
+
+// clientid: 1h7LPAAOs9AXxdeZYlsd9cvdFYjLMQ6N
