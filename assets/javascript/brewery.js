f32134e5454ef585567c2956567dc1d826b03834
Matt Wigdahl <matt@leverpulley.com>
2019-10-02 20:38:09 -0600

    worked on form submissions

--- assets/javascript/brewery.js
+++ assets/javascript/brewery.js
@@ -1,15 +1,18 @@
 // make ajax call
 var type = '';
-var state = 'utah';
-var city = 'salt lake city';
+var state = '';
+var city = '';
 var name = '';
 var tags = '';
+var pubs = [];
 
 function pubCrawl() {
   
   let queryURL = 'https://api.openbrewerydb.org/breweries' + 
   '?by_type=' + type + 
   '&by_state=' + state + 
+  '&by_name=' + name + 
+  '&by_name=' + tags + 
   '&by_city=' + city;
   $.ajax({
     url: queryURL,
@@ -20,3 +23,31 @@ function pubCrawl() {
 }
 
 pubCrawl();
+
+$('#by-city').on('click', function(event){
+  event.preventDefault();
+
+  var type = $('#by-type').val().trim();
+  var state = $('#by-state').val().trim();
+  var city = $('#by-city').val().trim();
+  var name = $('#by-name').val().trim();
+  var tags = $('#by-tags').val().trim();
+
+  console.log('type', type);
+  console.log('state', state);
+  console.log('city', city);
+  console.log('name', name);
+  console.log('tags', tags);
+
+
+  for (var i = 0; i < pubs.length; i++) {
+
+    $('#pubOutput').empty();
+
+    var pubResults = $('<div>');
+    pubResults.addClass('pubItem');
+    pubResults.text(pubs[i]);
+    pub.push(pubResults);
+  }
+
+});
