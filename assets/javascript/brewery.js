cdd22e1cd64395ab8ac64de8c092519358e3b790
Matt Wigdahl <matt@leverpulley.com>
2019-10-04 17:48:50 -0600

    Have basic pub profiles printing to pubOutput

--- assets/javascript/brewery.js
+++ assets/javascript/brewery.js
@@ -1,4 +1,3 @@
-var pubs = [];
 
 
 function buildPubQuery() {
@@ -21,6 +20,7 @@ function buildPubQuery() {
   '&by_name=' + name + 
   '&by_city=' + city;
   console.log('queryurl', queryURL);
+  return queryURL
 }
 
 //submit form data to build url query for api
@@ -33,11 +33,28 @@ $('#add-pubInput-btn').on('click', function(event){
     url: queryURL,
     method: 'GET'
 }).then(function (response){
-    var results = response;
-    console.log('results', results);
+    var pubs = response;
+    //console.log('pubs', pubs);
+    console.log('brewery name', pubs[0].name);
     
-    pubs.push(results);
+    pubs.push(pubs);
     console.log('pubs', pubs);
+    $('#pubOutput').empty();
+    for (var i = 0; i < pubs.length; i++) {
+
+      
+  //$('#pubOutput').empty();
+    
+      var pubResults = $('<div>');
+      pubResults.addClass('card card-body');
+      pubResults.html('<h3>' + pubs[i].name + '</h3>' + '<br/>' + 
+      'Street: ' + pubs[i].street + '<br/>' +
+      'Phone: ' + pubs[i].phone + '<br/>' + 
+      'Website: ' + pubs[i].website_url);
+      //pubResults.append(pubs[i]);
+      $('#pubOutput').prepend(pubResults);
+      //pubs.push(pubs);
+    }
   });
     
   });
@@ -50,7 +67,22 @@ $('#add-pubInput-btn').on('click', function(event){
     $('#pubOutput').append(pubCard);
   }
   
-  pubOutput()
+  brewery_type: "large"
+  city: "Boise"
+  country: "United States"
+  id: 2242
+  latitude: "43.6174335918367"
+  longitude: "-116.202360204082"
+  name: "10 Barrel Brewing Co"
+  phone: "5416785228"
+  postal_code: "83702-5857"
+  state: "Idaho"
+  street: "830 W Bannock St"
+  tag_list: []
+  updated_at: "2018-08-24T00:29:51.675Z"
+  website_url: "http://www.10barrel.com"
+
+  //pubOutput()
 // {/* <div class="card">
 //   <div class="card-body">
 //     <h5 class="card-title">Card title</h5>
@@ -61,15 +93,15 @@ $('#add-pubInput-btn').on('click', function(event){
 //   </div>
 // </div> */}
 
-// for (var i = 0; i < pubs.length; i++) {
+for (var i = 0; i < pubs.length; i++) {
 
-//   $('#pubOutput').empty();
+  $('#pubOutput').empty();
 
-//   var pubResults = $('<div>');
-//   pubResults.addClass('pubItem');
-//   pubResults.append(pubs[i]);
-//   //pubs.push(pubs);
-// }
+  var pubResults = $('<div>');
+  pubResults.addClass('pubItem');
+  pubResults.append(pubs[i]);
+  //pubs.push(pubs);
+}
 
 // console.log('pubs', pubs);
 
