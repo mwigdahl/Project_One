438f416bcdc2656fc106b76aca27683686f262b1
mwigdahl <matt@leverpulley.com>
2019-10-04 20:49:40 -0600

    added pub cards to index and started on firebase connection (#14)
    * adding function to print to DOM
    
    * adding some logs
    
    * Have basic pub profiles printing to pubOutput
    
    * cleaned up files and started firebase connection

--- assets/javascript/brewery.js
+++ assets/javascript/brewery.js
@@ -1,6 +1,19 @@
-var pubs = [];
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCDKdUg5y9iSENnaHRpGx2czlfEyjOza6s",
+  authDomain: "uber-pub-crawl.firebaseapp.com",
+  databaseURL: "https://uber-pub-crawl.firebaseio.com",
+  projectId: "uber-pub-crawl",
+  storageBucket: "uber-pub-crawl.appspot.com",
+  messagingSenderId: "746444227672",
+  appId: "1:746444227672:web:5938a0ac6de7275e42362d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+var database = firebase.database();
 
+//concatenate query url
 function buildPubQuery() {
 
   var type = $('#by-type').val().trim();
@@ -8,23 +21,19 @@ function buildPubQuery() {
   var city = $('#by-city').val().trim();
   var name = $('#by-name').val().trim();
   //var tags = '';
-  
-  console.log('type', type);
-  console.log('state', state);
-  console.log('city', city);
-  console.log('name', name);
 
-  
   let queryURL = 'https://api.openbrewerydb.org/breweries' + 
   '?by_type=' + type + 
   '&by_state=' + state + 
   '&by_name=' + name + 
   '&by_city=' + city;
-  console.log('queryurl', queryURL);
+
+  return queryURL
 }
 
-//submit form data to build url query for api
+//submit form input data
 $('#add-pubInput-btn').on('click', function(event){
+
   event.preventDefault();
   
   var queryURL = buildPubQuery();
@@ -34,15 +43,53 @@ $('#add-pubInput-btn').on('click', function(event){
     method: 'GET'
 }).then(function (response){
     var results = response;
+<<<<<<< HEAD
+    
+    var pubs = response;
+
+    // Set vars
+    var lat = '';
+    var long = '';
+    var city = '';
+    var state = '';
+    var street = '';
+    var zip = '';
+    
+    $('#pubOutput').empty();
+
+    // Create Pub result cards and print on page
+    for (var i = 0; i < pubs.length; i++) {
+=======
 
     console.log('results', results);
+>>>>>>> 392e9f3b31ef9204f714f6b6ccb52ed8a853c002
     
-    pubs.push(results);
-    console.log('pubs', pubs);
+      var pubResults = $('<div>');
+      pubResults.addClass('card card-body');
+      pubResults.html('<h3>' + pubs[i].name + '</h3>' +
+      'Street: ' + pubs[i].street + '<br/>' +
+      'Phone: ' + pubs[i].phone + '<br/>' + 
+      'Website: ' + pubs[i].website_url + 
+      '<button type="button" class="btn btn-primary">Add to PubCrawl</button>');
+
+      $('#pubOutput').prepend(pubResults);   
+      
+    }
+    // Not working but want to push selected pub to firebase
+        firebase.database('/pub').ref().push({
+          name: name,
+          street: street,
+          city: city,
+          state: state,
+          lat: lat,
+          long: long,
+        });
   });
     
   });
   
+<<<<<<< HEAD
+=======
   function pubOutput() {
     var pubCard = $('<div>');
     pubCard.addClass('card');
@@ -76,11 +123,19 @@ $('#add-pubInput-btn').on('click', function(event){
 // console.log('pubs', pubs);
 
 // for (var i = 0; i < pubs.length; i++) {
+>>>>>>> 392e9f3b31ef9204f714f6b6ccb52ed8a853c002
 
-//   //$('#pubOutput').empty();
-//   var pubResults = $('<div>');
-//   pubResults.addClass('pubItem');
-//   pubResults.text
-//   pubResults.append(pubs[i]);
-//   pubs.push(pubs);
-// }
+  // brewery_type: "large"
+  // city: "Boise"
+  // country: "United States"
+  // id: 2242
+  // latitude: "43.6174335918367"
+  // longitude: "-116.202360204082"
+  // name: "10 Barrel Brewing Co"
+  // phone: "5416785228"
+  // postal_code: "83702-5857"
+  // state: "Idaho"
+  // street: "830 W Bannock St"
+  // tag_list: []
+  // updated_at: "2018-08-24T00:29:51.675Z"
+  // website_url: "http://www.10barrel.com"
