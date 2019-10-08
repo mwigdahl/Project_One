864559f0227bdb1bd49d90388f0770fdc71bd2fb
KChristlieb <51836635+KChristlieb@users.noreply.github.com>
2019-10-07 18:03:46 -0600

    Kade (#35)
    * added Ajex with function call
    
    * got google api to work. Needs to have the map script on the HTML and code in the JS. No ajex is needed at this time.
    
    * I added styling for map size in the CSS file. Map is displaying however, errors happening when bewery api task are ran. Will work with Matt to fix.
    
    * Changed map to grab Geo location.
    
    * removed the second firebase command on the index file. should be working.
    
    *  updating changes
    
    * reapplied google code to display the map with a marker.
    
    * moved jqaury script to the top in the head.
    
    * fixed error with google maps and found why it would not display to the dom, there was a styling issue. Removed style.css and added the correct styling for maps in styles.css.
    
    * corrected the css file tag to styles.css.
    
    * changed map code to grab by laction. User has to accept location services.
    
    * found and coded a way to get pins to add together in a bunch. Can't seem to get a loop to work to grab lat lng. Cannot get the comment cards to display info on pins. Gets error that states it is not compatable.

--- assets/javascript/google.js
+++ assets/javascript/google.js
@@ -1,52 +1,96 @@
 $(document).ready(function(){
 
-//   function initMap() {
-//     // The location of Uluru
-//     var uluru = {lat: -25.344, lng: 131.036};
-//     // The map, centered at Uluru
-//     var map = new google.maps.Map(
-//         document.getElementById("googleMaps"), {zoom: 13, center: uluru});
-//     // The marker, positioned at Uluru
-//     var marker = new google.maps.Marker({position: uluru, map: map});
-//   }
+  function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('googleMaps'), {
+      zoom: 1,
+      center: {lat: 00, lng: 00}
+    });
+
+    // Create an array of alphabetical characters used to label the markers.
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var locations = [
+    {lat: 40.756391, lng: -111.8750211},
+    {lat: 40.698753, lng: -111.850318},
+    {lat: 40.751873, lng: -111.881724},
+    {lat: 40.755053, lng: -111.895509},
+    {lat: 40.750550, lng: -111.890674},
+    {lat: 40.7748784, lng: -111.89671},
+    {lat: 40.729823, lng: -111.948156},
+    {lat: 40.732671, lng: -111.899319}
+  ]
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    var markers = locations.map(function(location, i) {
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length]
+      });
+    });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+  }
+
+
+//  function initMap() {
+//    // The location of Uluru
+//    var location = [ 
+   
+
+//   //  {lat: , lng: },
+//   //  {lat: , lng: },
+//   //  {lat: , lng: },
+//   //  {lat: , lng: },
+//   //  {lat: , lng: },
+//    ]
+//    // The map, centered at Uluru
+//    var map = new google.maps.Map(
+//        document.getElementById("googleMaps"), {zoom: 13, center: {lat: -28.024, lng: 140.887}});
+//    // The marker, positioned at Uluru
+//    var marker = new google.maps.Marker({position: location, map: map});
+//  }
 // initMap()
 
-var map, infoWindow;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('googleMaps'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
-  });
-  infoWindow = new google.maps.InfoWindow;
+// var map, infoWindow;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('googleMaps'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 12
+//   });
+//   infoWindow = new google.maps.InfoWindow;
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+//   // Try HTML5 geolocation.
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       var pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//       };
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
+//       // infoWindow.setPosition(pos);
+//       // infoWindow.setContent('Location found.');
+//       // infoWindow.open(map);
+//       map.setCenter(pos);
+//     }, function() {
+//       handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   }
+// }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
-}
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ?
+//                         'Error: The Geolocation service failed.' :
+//                         'Error: Your browser doesn\'t support geolocation.');
+//   infoWindow.open(map);
+// }
 
-initMap()
+ initMap()
 });
